a7fa81dd86d322515125640704c34230fcf223e4
haeckdesign <webmaster@haeckdesign.com>
2023-11-12 20:21:37 -0500

    removed additional js holder

--- assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-{{!--  Include Additional Javascripts Below and Uncomment on default.hbs --}}
-
-(function ($, undefined) { "use strict";
-var $document = $(document);
-	$document.ready(function () {
-
-
-});
-})(jQuery);
